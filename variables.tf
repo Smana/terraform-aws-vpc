837f81e26de58a7841b5e08ff431c61061e40536
Smaine Kahlouch <smaine.kahlouch@dailymotion.com>
2020-12-26 19:18:20 +0100

    Changes for tf 0.14

--- variables.tf
+++ variables.tf
@@ -1,40 +1,40 @@
 # Tags
 
-variable tags {
-    description = "Different tag values which should be assigned to AWS resources created via Terraform)"
-    type = "map"
+variable "tags" {
+  description = "Different tag values which should be assigned to AWS resources created via Terraform)"
+  type        = map(any)
 }
 
 # AWS Regions / Zones
 
-variable aws_region {
-  type = "string"
+variable "aws_region" {
+  type        = string
   description = "AWS region which should be used"
 }
 
-variable aws_zones {
-  type = "list"
+variable "aws_zones" {
+  type        = list(string)
   description = "AWS AZs (Availability zones) where subnets should be created"
 }
 
 # Private subnets
 
-variable private_subnets {
+variable "private_subnets" {
   description = "Create both private and public subnets"
-  type = "string"
-  default = "false"
+  type        = string
+  default     = "false"
 }
 
 # Resource naming
 
-variable vpc_name {
+variable "vpc_name" {
   description = "Name of the VPC"
-  type = "string"
+  type        = string
 }
 
 # Network details (Change this only if you know what you are doing or if you think you are lucky)
 
-variable vpc_cidr {
-  type = "string"
+variable "vpc_cidr" {
+  type        = string
   description = "CIDR of the VPC"
 }
